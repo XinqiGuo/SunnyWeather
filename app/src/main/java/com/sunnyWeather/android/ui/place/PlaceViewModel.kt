07931deb60ac28b596ce3cfdf00518932851e5bf
Guoxinqi <284613424@qq.com>
2020-06-04 10:01:59 +0800

    切换城市

---
+++ app/src/main/java/com/sunnyWeather/android/ui/place/PlaceViewModel.kt
@@ -0,0 +1,27 @@
+package com.sunnyWeather.android.ui.place
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
+import androidx.lifecycle.ViewModel
+import com.sunnyWeather.android.logic.Repository
+import com.sunnyWeather.android.logic.model.Place
+
+class PlaceViewModel : ViewModel(){
+    private val searchLiveData = MutableLiveData<String>()
+
+    val placeList = ArrayList<Place>()
+
+    val placeLiveData = Transformations.switchMap(searchLiveData){query ->
+        Repository.searchPlaces(query)
+    }
+
+    fun searchPlaces(query:String){
+        searchLiveData.value = query
+    }
+
+    fun savePlace(place: Place) = Repository.savePlace(place)
+
+    fun getSavePlace() = Repository.getSacedPlace()
+
+    fun isPlaceSaved() = Repository.isPlaceSaced()
+}
